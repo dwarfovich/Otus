ca743c70214ed949bf96cadb6e241d6fb7adf654
Tim Gilevich <ouncetim@gmail.com>
2024-07-02 00:41:41 +0300

    WIP

--- HT1/build.bat
+++ HT1/build.bat
@@ -1,15 +1,4 @@
 @echo off
 
-for %%I in (.) do set CurrDirName=%%~nxI
-if %CurrDirName% neq "Build" (
-  mkdir Build
-)
-rem cd Build
-
-rem cmake ..
-rem cmake --build .
 cmake -BBuild-MSVC
 cmake --build Build-MSVC
-rem -S/path/to/my/source/folder
-
-rem cd ..
