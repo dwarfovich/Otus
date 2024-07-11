f9ec967c50d9afdebdd1018411519e63ddaac852
Tim Gilevich <ouncetim@gmail.com>
2024-07-11 18:30:40 +0300

    WIP

--- HT2/HT2/ip.h
+++ HT2/HT2/ip.h
@@ -15,12 +15,14 @@ public:
     friend std::ostream &operator<<(std::ostream &input, const Ip &ip);
     friend std::istream &operator>>(std::istream &input, Ip &ip);
 
-    constexpr bool operator==(const Ip &rhs) const { return values_ == rhs.values_; }
-    constexpr bool operator!=(const Ip &rhs) const { return values_ != rhs.values_; }
-    constexpr bool operator<(const Ip &rhs) const { return values_ < rhs.values_; }
-    constexpr bool operator<=(const Ip &rhs) const { return values_ <= rhs.values_; }
-    constexpr bool operator>(const Ip &rhs) const { return values_ > rhs.values_; }
-    constexpr bool operator>=(const Ip &rhs) const { return values_ >= rhs.values_; }
+    constexpr uint8_t       &operator[](size_t i) { return values_[i]; }
+    constexpr const uint8_t &operator[](size_t i) const { return values_[i]; }
+    constexpr bool           operator==(const Ip &rhs) const { return values_ == rhs.values_; }
+    constexpr bool           operator!=(const Ip &rhs) const { return values_ != rhs.values_; }
+    constexpr bool           operator<(const Ip &rhs) const { return values_ < rhs.values_; }
+    constexpr bool           operator<=(const Ip &rhs) const { return values_ <= rhs.values_; }
+    constexpr bool           operator>(const Ip &rhs) const { return values_ > rhs.values_; }
+    constexpr bool           operator>=(const Ip &rhs) const { return values_ >= rhs.values_; }
 
 private:
     std::array<uint8_t, partsCount> values_ = {};
--- HT2/HT2/main.cpp
+++ HT2/HT2/main.cpp
@@ -13,39 +13,73 @@ std::pair<std::vector<ip::Ip>, bool> readIps(std::istream& stream);
 int main(int argc, char* argv[])
 {
 #ifdef _DEBUG
-    std::ifstream stream {"../../../ip_filter.tsv"};
+    std::ifstream stream { "../../../ip_filter.tsv" };
+#else
+    std::istream& stream = std::cin;
 #endif // _DEBUG
-
-    auto [ips, success] = readIps(stream);
-    if(!success){
-        std::cerr << "Couldn't parse some IPs. Quiting."<< std::endl;
+    if (!stream.is_open()) {
+        std::cerr << "Failed to open input stream\n";
         return 1;
     }
-    
+
+#ifdef _DEBUG
+    std::ofstream outStream { "../../../result.tsv" };
+#else
+    std::ostream& outStream = std::cout;
+#endif // _DEBUG
+    if (!outStream.is_open()) {
+        std::cerr << "Failed to open output stream\n";
+        return 1;
+    }
+
+    auto [ips, success] = readIps(stream);
+    if (!success) {
+        std::cerr << "Couldn't parse some IPs. Quiting." << std::endl;
+        return 1;
+    }
+
     using IpsIterator = decltype(ips)::iterator;
     std::vector<std::function<void(IpsIterator begin, IpsIterator end)>> jobs;
-    jobs.push_back([](IpsIterator begin, IpsIterator end){
+    jobs.push_back([&outStream](IpsIterator begin, IpsIterator end) {
         std::sort(begin, end, std::greater());
+        std::for_each(begin, end, [&outStream](const auto& ip) {
+            outStream << ip << '\n';
         });
+    });
+    jobs.push_back([&outStream](IpsIterator begin, IpsIterator end) {
+        std::for_each(begin, end, [&outStream](const auto& ip) {
+            if (ip[0] == 1) {
+                outStream << ip << '\n';
+            }
+        });
+    });
+    jobs.push_back([&outStream](IpsIterator begin, IpsIterator end) {
+        std::for_each(begin, end, [&outStream](const auto& ip) {
+            if (ip[0] == 46 && ip[1] == 70) {
+                outStream << ip << '\n';
+            }
+        });
+    });
 
+    jobs.push_back([&outStream](IpsIterator begin, IpsIterator end) {
+        std::for_each(begin, end, [&outStream](const auto& ip) {
+            if (ip[0] == 46 || ip[1] == 46 || ip[2] == 46 || ip[3] == 46) {
+                outStream << ip << '\n';
+            }
+        });
+    });
 
-
-    for(const auto& job : jobs){
+    for (const auto& job : jobs) {
         job(ips.begin(), ips.end());
-    
-    
     }
-    for (const auto& ip : ips){
-        std::cout << ip << std::endl;
-    }
-
+    
     return 0;
 }
 
 std::pair<std::vector<ip::Ip>, bool> readIps(std::istream& stream)
 {
     std::pair<std::vector<ip::Ip>, bool> result;
-    ip::Ip ip;
+    ip::Ip                               ip;
     while (stream >> ip) {
         result.first.push_back(std::move(ip));
         constexpr auto noLimit = std::numeric_limits<std::streamsize>::max();
