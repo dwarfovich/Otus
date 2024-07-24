b00af1f187df387ce4548a10517668dd37118a92
Tim Gilevich <ouncetim@gmail.com>
2024-07-24 17:30:11 +0300

    WIP

--- HW4/include/print_ip.h
+++ HW4/include/print_ip.h
@@ -1,3 +1,29 @@
 #pragma once
 
-inline int f(){ return 8;}
+#include <iostream>
+
+template<typename T, std::enable_if_t<std::is_integral_v<T>, bool> = false>
+void print_ip(const T& v)
+{
+    std::cout << "integral: " << v << std::endl;
+}
+
+template<typename T, std::enable_if_t<std::is_same_v<std::string, T>, bool> = false>
+void print_ip(const T& str)
+{
+    std::cout << "string: " << str << std::endl;
+}
+
+template<typename T, std::enable_if_t<(sizeof(T::const_iterator) > 0) && !std::is_same_v<std::string, T>, bool> = false>
+void print_ip(const T& c)
+{
+    if(c.empty()){
+        return;
+    }
+
+    std::cout << *c.cbegin();
+    for (auto i = ++c.cbegin(); i != c.cend(); ++i){
+        std::cout << '.' << *i;
+    }
+    std::cout << std::endl;
+}
--- HW4/src/main.cpp
+++ HW4/src/main.cpp
@@ -1,10 +1,19 @@
 ﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
 
 #include "print_ip.h"
 
 int main() {
 
-    std::cout << f() << std::endl;
+    print_ip(14);
+    print_ip(char(1));
+    print_ip(std::string("123"));
+    std::vector<int> v {44,55,66};
+    print_ip(v);
+    std::list<int> l { 77, 88, 99, 909 };
+    print_ip(l);
 
     return 0;
 }
