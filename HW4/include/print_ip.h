781acfa83dc2597d4f75c1e3cfcf732c10aff60d
Tim Gilevich <ouncetim@gmail.com>
2024-07-27 10:23:26 +0300

    Fix printing

--- HW4/include/print_ip.h
+++ HW4/include/print_ip.h
@@ -1,17 +1,28 @@
 #pragma once
 
 #include <iostream>
+#include <tuple>
+#include <climits>
 
 template<typename T, std::enable_if_t<std::is_integral_v<T>, bool> = false>
 void print_ip(const T& v)
 {
-    std::cout << "integral: " << v << std::endl;
+    std::cout << (v >> (sizeof(v) - 1) * CHAR_BIT);
+    if (sizeof(v) == 1) {
+        return;
+    }
+
+    for (int i = static_cast<int>(sizeof(v)) - 2; i >= 0; --i) {
+        const auto shift = static_cast<T>(i * CHAR_BIT);
+        const auto mask  = static_cast<T>(0xFF) << shift;
+        std::cout << '.' << ((v & mask) >> shift);
+    }
 }
 
 template<typename T, std::enable_if_t<std::is_same_v<std::string, T>, bool> = false>
 void print_ip(const T& str)
 {
-    std::cout << "string: " << str << std::endl;
+    std::cout << str;
 }
 
 template<typename T, std::enable_if_t<(sizeof(T::const_iterator) > 0) && !std::is_same_v<std::string, T>, bool> = false>
@@ -25,35 +36,41 @@ void print_ip(const T& c)
     for (auto i = ++c.cbegin(); i != c.cend(); ++i) {
         std::cout << '.' << *i;
     }
-    std::cout << std::endl;
 }
 
 namespace details {
 
-template<size_t I = 0, typename... Ts, typename std::enable_if_t<I == 0, bool>  = false>
-void printTuple(const std::tuple<Ts...>& tuple)
+template<typename Head, typename... Tail>
+constexpr bool tuple_has_equal_types()
 {
-    std::cout << std::get<0>(tuple);
-    printTupleAfter0<I + 1>(tuple);
+    return (std::is_same_v<Head, Tail> && ...);
 }
 
-template<size_t I, typename... Ts, typename std::enable_if_t<I == sizeof...(Ts), bool> = false>
-void printTupleAfter0(const std::tuple<Ts...>& tuple)
+template<size_t I = 0, typename... Ts, std::enable_if_t<I == 0, bool> = false>
+void print_tuple(const std::tuple<Ts...>& tuple)
+{
+    std::cout << std::get<0>(tuple);
+    print_tuple_except_first_element<I + 1>(tuple);
+}
+
+template<size_t I, typename... Ts, std::enable_if_t<I == sizeof...(Ts), bool> = false>
+void print_tuple_except_first_element(const std::tuple<Ts...>& tuple)
 {
     return;
 }
 
-template<size_t I, typename... Ts, typename std::enable_if_t<(I < sizeof...(Ts)), bool> = false>
-void printTupleAfter0(const std::tuple<Ts...>& tuple)
+template<size_t I, typename... Ts, std::enable_if_t<(I < sizeof...(Ts)), bool> = false>
+void print_tuple_except_first_element(const std::tuple<Ts...>& tuple)
 {
     std::cout << '.' << get<I>(tuple);
-    printTupleAfter0<I + 1>(tuple);
+    print_tuple_except_first_element<I + 1>(tuple);
 }
 
 } // namespace details
 
-template<typename T, std::enable_if_t<(std::tuple_size<T> {} > 0), bool> = false>
-void print_ip(const T& t)
+template<typename... Ts, std::enable_if_t<(sizeof...(Ts) > 0), bool> = false>
+void print_ip(const std::tuple<Ts...>& tuple)
 {
-    details::    printTuple(t);
+    static_assert(details::tuple_has_equal_types<Ts ...>() && "tuple has to has elements of the single type");
+    details::print_tuple(tuple);
 }
--- HW4/src/main.cpp
+++ HW4/src/main.cpp
@@ -5,18 +5,26 @@
 
 #include "print_ip.h"
 
-int main() {
-    print_ip(14);
-    print_ip(char(1));
-    print_ip(std::string("123"));
-    std::vector<int> v {44,55,66};
+template<typename T>
+void print_ip_with_endl(const T& v)
+{
     print_ip(v);
+    std::cout << std::endl;
+}
+
+int main() {
+    print_ip_with_endl(2130706433);
+    print_ip_with_endl(char(1));
+    using namespace std::string_literals;
+    print_ip_with_endl("Hello, world!"s);
+    std::vector<int> v {44,55,66};
+    print_ip_with_endl(v);
     std::list<int> l { 77, 88, 99, 909 };
-    print_ip(l);
+    print_ip_with_endl(l);
     std::tuple t {9,8,7,6,5};
-    print_ip(t);
-    std::tuple t2 { 9, true };
-    print_ip(t2);
+    print_ip_with_endl(t);
+    //std::tuple t2 { 9, true };
+    //print_ip(t2);
 
     return 0;
 }
