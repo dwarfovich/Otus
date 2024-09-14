c0d6a9b608b0e043304a40b71cff9f45a41f6036
Tim Gilevich <ouncetim@gmail.com>
2024-09-14 14:01:45 +0300

    WIP

--- HW3/src/main.cpp
+++ HW3/src/main.cpp
@@ -11,7 +11,7 @@ template<typename T>
 T factorial(T n)
 {
     if (n < 0) {
-        return 0;
+        throw std::invalid_argument("n have to be greater or equal to 0");
     }
 
     if (n == 0 || n == 1) {
@@ -48,7 +48,26 @@ int main()
     }
 
     cc::ForwardList<int> list;
-    
+    auto iter = list.cbefore_begin();
+    for(int i = 0; i < 10; ++i){
+        iter = list.insert_after(iter, factorial(i));
+    }
+    std::cout << "Custom container with std::allocator:\n";
+    int counter = 0;
+    for( auto i : list){
+        std::cout << counter++ << ": " << i << '\n';
+    }
+
+    cc::ForwardList<int, MMA> mmList;
+    auto                      mmIter = mmList.cbefore_begin();
+    for (int i = 0; i < 10; ++i) {
+        mmIter = mmList.insert_after(mmIter, factorial(i));
+    }
+    std::cout << "Custom container with custom allocator:\n";
+    int mmCounter = 0;
+    for (auto i : mmList) {
+        std::cout << mmCounter++ << ": " << i << '\n';
+    }
 
     return 0;
 }
