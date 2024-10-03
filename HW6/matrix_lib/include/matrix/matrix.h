65242455847295ff856ddd30f8221a73a894cdee
Tim Gilevich <ouncetim@gmail.com>
2024-10-04 01:25:23 +0300

    WIP

--- HW6/matrix_lib/include/matrix/matrix.h
+++ HW6/matrix_lib/include/matrix/matrix.h
@@ -5,91 +5,110 @@
 #include <cstddef>
 #include <unordered_map>
 #include <array>
-
-template<typename ElementType, std::size_t Dimension, typename Hasher>
-class Matrix;
-
-//template<template<class, class> class V, class T, class A>
-//void f(V<T, A>& v)
-//{
-//    // This can be "typename V<T, A>::value_type",
-//    // but we are pretending we don't have it
-//
-//    T temp = v.back();
-//    v.pop_back();
-//    // Do some work on temp
-//
-//    std::cout << temp << std::endl;
-//}
-
-template<typename ElementType, std::size_t Dimension, typename Hasher>
-struct IBase
-{
-    IBase<ElementType, Dimension, Hasher>& operator[](std::size_t index)
-    {
-        position_[posIndex_] = index;
-        ++posIndex_;
-        return *this;
-    }
-
-    IBase<ElementType, Dimension, Hasher>& operator=(const ElementType& element)
-    {
-        matrix_.set(position_, element);
-
-        return *this;
-        //return *this;
-    }
-
-    size_t posIndex_ = 0;
-    mutable std::array<std::size_t, Dimension> position_;
-    Matrix<ElementType, Dimension, Hasher>&    matrix_;
-};
+#include <stdexcept>
 
 template<typename ElementType, std::size_t Dimension = 2, typename Hasher = MatrixPositionHasher<Dimension>>
-class Matrix
+class SparseMatrix
 {
-public:
-    IBase<ElementType, Dimension, Hasher> operator[](std::size_t index) { return {0ul, {}, *this}; }
-
-    class ElementProxyRef
+public: // types
+    class ElementProxy
     {
-    public:
-        using MatrixType = Matrix<ElementType, Dimension>;
+    public: // types
+        using MatrixType = SparseMatrix<ElementType, Dimension, Hasher>;
 
-        ElementProxyRef(MatrixType& matrix, ElementType* element = nullptr) : matrix_ { matrix }, element_ { element }
+    public:
+        ElementProxy(MatrixType& matrix) : currentOrdinal_ { 0 }, matrix_ { matrix } {
+            position_.back() = invalidOrdinal_;
+        }
+        ElementProxy(const ElementProxy& rhs)
+            : currentOrdinal_ { invalidOrdinal_ }, position_ { rhs.position_ }, matrix_ { rhs.matrix_ }
         {
         }
 
-    private:
-        ElementType* element_ = nullptr;
-        MatrixType&  matrix_;
+        operator ElementType() { return matrix_.copyElement(position_); }
+
+        ElementProxy& operator[](std::size_t index)
+        {
+            throwIfOrdinalIsOutOfRange();
+            position_[currentOrdinal_++] = index;
+
+            return *this;
+        }
+
+        ElementProxy& operator=(const ElementType& element)
+        {
+            throwIfOrdinalIsOutOfRange();
+            throwIfPositionIsInvalid();
+
+            matrix_.set(position_, element);
+
+            return *this;
+        }
+
+        ElementType  element() const noexcept { return matrix_.copyElement(position_); }
+        ElementType& elementRef() const noexcept { return matrix_.trueRef(position_); }
+
+    private: // methods
+        void throwIfOrdinalIsOutOfRange()
+        {
+            if (currentOrdinal_ >= Dimension) {
+                throw std::logic_error("It is forbidden to assign a value to copy-constructed proxy element and/or "
+                                       "increment ordinal above Dimension.");
+            }
+        }
+        void throwIfPositionIsInvalid() {
+            if (position_.back() == invalidOrdinal_) {
+                throw std::logic_error("Position is invalid.");
+            }
+        }
+
+    private: // data
+        size_t                                     currentOrdinal_ = 0;
+        MatrixType&                                matrix_;
+        mutable std::array<std::size_t, Dimension> position_ = {};
     };
 
     using Position = std::array<std::size_t, Dimension>;
 
-    Matrix(const ElementType& defaultElement = {}) : defaultElement_ { defaultElement } {}
+public: // methods
+    SparseMatrix(const ElementType& defaultElement = -1) : defaultElement_ { defaultElement } {}
+
+    ElementProxy operator[](std::size_t index) { return { *this }; }
 
     const ElementType& defaultElement() const noexcept { return defaultElement_; }
-    ElementProxyRef    refAt(const Position& position) const noexcept;
-    void               set(const Position& position, const ElementType& element) {
-        elements_[position] = element;
+    // ElementProxy       proxyAt(const Position& position) const noexcept;
+    void set(const Position& position, const ElementType& element) { elements_[position] = element; }
+    void set(const Position& position, ElementType&& element) { elements_[position] = element; }
+
+    ElementType& trueRef(const Position& position) const noexcept
+    {
+        auto iter = elements_.find(position);
+        if (iter == elements_.cend()) {
+            return defaultElement_;
+        } else {
+            return iter->second;
+        }
     }
-    void               set(const Position& position, ElementType&& element) { elements_[position] = element; }
+    ElementType copyElement(const Position& position) const noexcept { return trueRef(position); }
+
+public: // data
+    static constexpr std::size_t invalidOrdinal_ = -1;
 
 private:
     using ElementsContainer = std::unordered_map<Position, ElementType, Hasher>;
+
     ElementsContainer elements_;
     ElementType       defaultElement_;
 };
 
-template<typename ElementType, std::size_t Dimension, typename Hasher>
-auto Matrix<ElementType, Dimension, Hasher>::refAt(const Position& position) const noexcept -> ElementProxyRef
-{
-    ElementProxyRef element { *this };
-    auto            iter = elements_.find(position);
-    if (iter == elements_.cend()) {
-        return { *this, &defaultElement_ };
-    } else {
-        return { *this, &iter->second };
-    }
-}
+// template<typename ElementType, std::size_t Dimension, typename Hasher>
+// auto SparseMatrix<ElementType, Dimension, Hasher>::proxyAt(const Position& position) const noexcept -> ElementProxy
+//{
+//     ElementProxy element { *this };
+//     auto         iter = elements_.find(position);
+//     if (iter == elements_.cend()) {
+//         return { *this, &defaultElement_ };
+//     } else {
+//         return { *this, &iter->second };
+//     }
+// }
--- HW6/src/main.cpp
+++ HW6/src/main.cpp
@@ -4,10 +4,29 @@
 
 int main()
 {
-    Matrix<int, 2> m (-1);
+    SparseMatrix<int, 2> m (-1);
     auto t = m[0][3];
-    t = 5;
-   m[0][1] = 45;
+    try{
+        t = 5;
+    } catch(const std::exception& e){
+        std::cout << "!!! t = 5;" <<'\n';
+        std::cout << "Exception! " << e.what() << '\n';
+    }
+    const auto ct = m[4][432];
+    // ct = 5;
+
+    try {
+        m[123] = 43;
+    } catch (const std::exception& e) {
+        std::cout << "!!! m[123] = 43;" << '\n';
+        std::cout << "Exception! " << e.what() << '\n';
+    }
+
+    
+
+    //t = 5;
+   //m[0][1] = 45;
+   //int tint = m[0][0];
 
     return 0;
 }
