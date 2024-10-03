28441182b780ffa0e12d5591b2afac2aed8d7401
Tim Gilevich <ouncetim@gmail.com>
2024-10-03 11:51:23 +0300

    WIP

--- HW6/matrix_lib/include/matrix/matrix.h
+++ HW6/matrix_lib/include/matrix/matrix.h
@@ -4,10 +4,60 @@
 #include <unordered_map>
 #include <array>
 
-template<typename ElementType, std::size_t Dimension = 2, ElementType DeafaultElement = ElementType()>
+// template<typename ElementType>
+// class ElementProxyRef
+//{
+//     template<typename ElementType, std::size_t Dimension, ElementType DefaultElement>
+//     friend class Matrix;
+//
+// public:
+//
+// private:
+//     ElementType* element_ = nullptr;
+//     Matrix<ElementType>& matrix_;
+// };
+//
+// template<typename ElementType, std::size_t Dimension = 2>
+// class ElementProxy
+//{
+//     template<typename ElementType, std::size_t Dimension, ElementType DefaultElement>
+//     friend class Matrix;
+//
+// public:
+// private:
+//     ElementType element_;
+// };
+
+template<typename ElementType, std::size_t Dimension = 2, ElementType DefaultElement = ElementType()>
 class Matrix
 {
 public:
+    class ElementProxyRef
+    {
+    public:
+        ElementProxyRef(Matrix<ElementType, Dimension, DefaultElement>& matrix) : matrix_{matrix}{}
+    private:
+        ElementType*                                    element_ = nullptr;
+        Matrix<ElementType, Dimension, DefaultElement>& matrix_;
+    };
+
+    using Position = std::array<std::size_t, Dimension>;
+
+    ElementProxyRef refAt(const Position& position) const noexcept;
+
 private:
-    std::unordered_map<std::array<std::size_t, Dimension> , ElementType> elements_;
+    std::unordered_map<Position, ElementType> elements_;
 };
+
+template<typename ElementType, std::size_t Dimension, ElementType DefaultElement>
+auto Matrix<ElementType, Dimension, DefaultElement>::refAt(const Position& position) const noexcept -> ElementProxyRef
+{
+    auto iter = elements_.find(position);
+    if (iter == elements_.cend()) {
+        //cachedElement_.element_ = &DefaultElement;
+    } else {
+        //cachedElement_.element = &iter->second;
+    }
+
+    return {};
+}
