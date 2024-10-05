83c527050d3584928732e863d65e532b0c874659
Tim Gilevich <ouncetim@gmail.com>
2024-10-05 11:25:33 +0300

    WIP

--- HW6/src/main.cpp
+++ HW6/src/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include "matrix/matrix.h"
 
 #include <iostream>
+#include <iomanip>
+#include <cassert>
 
 int main()
 {
@@ -37,6 +39,48 @@ int main()
         std::cout << ": " << element << std::endl;
     }
 
+    std::cout << '\n';
+    SparseMatrix<int> matrix { -1 };
+    assert(matrix.size() == 0); // все ячейки свободны
+    auto a = matrix[0][0];
+    assert(a == -1);
+    assert(matrix.size() == 0);
+    matrix[100][100] = 314;
+    assert(matrix[100][100] == 314);
+    assert(matrix.size() == 1);
+    for (const auto& c : matrix) {
+        decltype(matrix)::Position position;
+        decltype(matrix)::Element  element;
+        std::tie(position, element) = c;
+
+        printPosition(std::cout, position);
+        std::cout << ": " << element << std::endl;
+    }
+
+    std::cout << '\n';
+    std::cout << "Homework\n";
+    SparseMatrix<int> mmm { 0 };
+    for (std::size_t i = 0 ; i < 10; ++i){
+        mmm[i][i] = i;
+        mmm[i][9-i] = 10-1 - i;
+    }
+    for (std::size_t i = 1; i < 9; ++i) {
+        for (std::size_t j = 1; j < 9; ++j) {
+            std::cout << std::setw(2) <<mmm[i][j].element() << "  ";
+        }
+        std::cout << '\n';
+    }
+
+    std::cout <<"Elements count: " << mmm.size() << '\n';
+    std::cout << "Elements: \n";
+    for (const auto& c : mmm) {
+        decltype(mmm)::Position position;
+        decltype(mmm)::Element  element;
+        std::tie(position, element) = c;
+
+        printPosition(std::cout, position);
+        std::cout << ": " << element << std::endl;
+    }
 
     return 0;
 }
