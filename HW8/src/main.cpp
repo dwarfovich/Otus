97441cfb0b783306b3aef99f26c9c96b2a642828
Tim Gilevich <ouncetim@gmail.com>
2024-10-14 19:13:30 +0300

    WIP

--- HW8/src/main.cpp
+++ HW8/src/main.cpp
@@ -1,6 +1,33 @@
 ﻿#include <boost/program_options.hpp>
+#include <boost/version.hpp>
+
+#include "cryptopp/cryptlib.h"
+
+#include <cryptopp/sha3.h>
+#include <cryptopp/hex.h>
 
 #include <iostream>
+
+
+std::string sha3(std::string& input)
+{
+    CryptoPP::SHA3_256 hash;
+
+    std::string digest;
+    std::string output;
+
+    CryptoPP::StringSource(input,
+                           true,
+                           new CryptoPP::HashFilter(hash,
+                                                    new CryptoPP::StringSink(digest)) // HashFilter
+    );                                                                                // StringSource
+
+    CryptoPP::StringSource(digest, true,
+                           new CryptoPP::HexEncoder(new CryptoPP::StringSink(output))); // StringSource
+
+    return output;
+}
+
 int main(int argc, char* argv[])
 {
     namespace  po= boost::program_options;
@@ -21,5 +48,30 @@ int main(int argc, char* argv[])
     } else {
         std::cout << "Compression level was not set.\n";
     }
+
+    std::cout << "Using Boost " << BOOST_VERSION / 100000 << "." // major version
+              << BOOST_VERSION / 100 % 1000 << "."               // minor version
+              << BOOST_VERSION % 100                             // patch level
+              << std::endl;
+
+    std::string msg1 = "Lorem ipsum dolor sit amet conse";
+    std::string msg2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,";
+    std::string msg3 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, neque exercitationem? Et iusto "
+                       "veniam nostrum voluptatem dolor, m";
+    std::string msg4 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, neque exercitationem? Et iusto "
+                       "veniam nostrum voluptatem dolor, maxime deleniti harum aperiam molestias animi quam assumenda "
+                       "ipsam repellat earum ab quae. Lorem ipsum dolor sit amet consectetur";
+    std::string msg5 =
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, neque exercitationem? Et iusto veniam nostrum "
+        "voluptatem dolor, maxime deleniti harum aperiam molestias animi quam assumenda ipsam repellat earum ab quae. "
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, neque exercitationem? Et iusto veniam nostrum "
+        "voluptatem dolor, maxime deleniti harum aperiam molestias animi quam assumenda ipsam repellat earum ab quae. "
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, neque exerci";
+    std::string digest;
+
+        digest = sha3(msg1);
+    std::cout << "Message: " << msg1 << std::endl;
+    std::cout << "Digest: " << digest << std::endl;
+
     return 0;
 }
