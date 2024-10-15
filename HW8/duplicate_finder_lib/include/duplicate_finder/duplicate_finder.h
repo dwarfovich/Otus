cf073534df47f5b2529007e602838b7522685eb4
Tim Gilevich <ouncetim@gmail.com>
2024-10-15 19:11:50 +0300

    WIP

---
+++ HW8/duplicate_finder_lib/include/duplicate_finder/duplicate_finder.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class DuplicateFinder
+{
+public:
+
+    private:
+
+};
--- HW8/src/main.cpp
+++ HW8/src/main.cpp
@@ -1,8 +1,9 @@
-﻿#include <boost/program_options.hpp>
+﻿#include "duplicate_finder/Header1.h"
+
+#include <boost/program_options.hpp>
 #include <boost/version.hpp>
 
-#include "cryptopp/cryptlib.h"
-
+#include <cryptopp/cryptlib.h>
 #include <cryptopp/sha3.h>
 #include <cryptopp/hex.h>
 
@@ -30,48 +31,6 @@ std::string sha3(std::string& input)
 
 int main(int argc, char* argv[])
 {
-    namespace  po= boost::program_options;
-    po::options_description description("Allowed options");
-    description.add_options()("help", "produce help message")("compression", po::value<int>(), "set compression level");
-
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, description), vm);
-    po::notify(vm);
-
-    if (vm.count("help")) {
-        std::cout << description << "\n";
-        return 1;
-    }
-
-    if (vm.count("compression")) {
-        std::cout << "Compression level was set to " << vm["compression"].as<int>() << ".\n";
-    } else {
-        std::cout << "Compression level was not set.\n";
-    }
-
-    std::cout << "Using Boost " << BOOST_VERSION / 100000 << "." // major version
-              << BOOST_VERSION / 100 % 1000 << "."               // minor version
-              << BOOST_VERSION % 100                             // patch level
-              << std::endl;
-
-    std::string msg1 = "Lorem ipsum dolor sit amet conse";
-    std::string msg2 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,";
-    std::string msg3 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, neque exercitationem? Et iusto "
-                       "veniam nostrum voluptatem dolor, m";
-    std::string msg4 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, neque exercitationem? Et iusto "
-                       "veniam nostrum voluptatem dolor, maxime deleniti harum aperiam molestias animi quam assumenda "
-                       "ipsam repellat earum ab quae. Lorem ipsum dolor sit amet consectetur";
-    std::string msg5 =
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, neque exercitationem? Et iusto veniam nostrum "
-        "voluptatem dolor, maxime deleniti harum aperiam molestias animi quam assumenda ipsam repellat earum ab quae. "
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, neque exercitationem? Et iusto veniam nostrum "
-        "voluptatem dolor, maxime deleniti harum aperiam molestias animi quam assumenda ipsam repellat earum ab quae. "
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, neque exerci";
-    std::string digest;
-
-        digest = sha3(msg1);
-    std::cout << "Message: " << msg1 << std::endl;
-    std::cout << "Digest: " << digest << std::endl;
 
     return 0;
 }
