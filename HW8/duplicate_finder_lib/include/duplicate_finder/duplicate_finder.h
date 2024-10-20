40fe9c9c10ae5cee78c425ed42cb9b30b37c5b3c
Tim Gilevich <ouncetim@gmail.com>
2024-10-20 18:41:15 +0300

    WIP

--- HW8/duplicate_finder_lib/include/duplicate_finder/duplicate_finder.h
+++ HW8/duplicate_finder_lib/include/duplicate_finder/duplicate_finder.h
@@ -3,18 +3,48 @@
 #include "path_vector.h"
 #include "duplicates.h"
 #include "finder_task.h"
+#include "file_finder.h"
 #include "signal.h"
+
+
+#include <boost/thread.hpp>
+#include <boost/asio/post.hpp>
+#include <boost/asio/thread_pool.hpp>
 
 class DuplicateFinder
 {
+private: // types
+    using ThreadPoolPtr = std::shared_ptr<boost::asio::thread_pool>;
+
 public: // methods
-    void findDuplicates(const FinderTask& task) {}
+    void findDuplicates(const FinderTask& task)
+    {
+        threadPool_->stop();
+        currentTask_ = task;
+        auto newSize = task.threadCount == 0 ? boost::thread::hardware_concurrency() : task.threadCount;
+        if (threadPoolSize_ == 0 || threadPoolSize_ != newSize) {
+            threadPool_     = std::make_shared<boost::asio::thread_pool>(newSize);
+            threadPoolSize_ = newSize;
+        }
+        startWork();
+    }
 
 public: // signals
     Signal<void()> taskFinished;
 
-private:
+private: // methods
+    void startWork() {
+        boost::asio::post(*threadPool_, [this](){
+            fileFinder_.findFiles(currentTask_);
+            });
+    }
 
-    FinderTask currentTask_;
-    Duplicates duplicates_;
+private: // data
+    FinderTask    currentTask_;
+    Duplicates    duplicates_;
+    FileFinder  fileFinder_;
+    unsigned      threadPoolSize_ = 0;
+    ThreadPoolPtr threadPool_;
+
+    static inline constexpr std::size_t minimumFilesPerThread_ = 20;
 };
--- HW8/duplicate_finder_lib/include/duplicate_finder/finder_task.h
+++ HW8/duplicate_finder_lib/include/duplicate_finder/finder_task.h
@@ -10,10 +10,11 @@
 struct FinderTask
 {
     std::shared_ptr<Digester> digester = nullptr;
-    PathVector targets;
-    PathVector blackList;
-    bool recursiveSearch = false;
-    std::size_t minimalFileSize = 1;
-    StringVector extensionsMasks;
-    std::size_t blockSize = 1024;
+    PathVector                targets;
+    PathVector                blackList;
+    bool                      recursiveSearch = false;
+    std::size_t               minimalFileSize = 1;
+    StringVector              extensionsMasks;
+    std::size_t               blockSize   = 1024;
+    unsigned               threadCount = 1;
 };
