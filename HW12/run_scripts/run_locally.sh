c745958204c5bcf3e9d6d3aab3425a3bdb4400a6
Tim Gilevich <ouncetim@gmail.com>
2024-11-25 07:01:32 +0300

    WIP

--- HW12/run_scripts/run_locally.sh
+++ HW12/run_scripts/run_locally.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
+echo Calculating mean...
 ./map_mean < AB_NYC_2019.csv | ./reduce_mean > output_mean.txt
 echo Result written into /opt/map_reduce/output_mean.txt
+
+echo Calculating dispersion...
+./map_dispersion < AB_NYC_2019.csv | ./reduce_dispersion > output_dispersion.txt
+echo Result written into /opt/map_reduce/output_dispersion.txt
