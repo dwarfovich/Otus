fcc2c3e4b2c1808a92d5c1e4a86bd8ace392d8bb
Tim Gilevich <ouncetim@gmail.com>
2024-12-07 19:58:01 +0300

    WIP

--- HW13/driver/main.cpp
+++ HW13/driver/main.cpp
@@ -17,23 +17,23 @@ Matrix readCoefficientsMatrix(const std::filesystem::path& file, const std::stri
     std::ifstream stream { file };
     std::string   line;
     Matrix        m;
-    
+
     while (std::getline(stream, line)) {
         std::vector<std::string> tokens;
         boost::split(tokens, line, boost::is_any_of(delimiter));
         Row row(tokens.size());
         std::transform(tokens.cbegin(), tokens.cend(), row.begin(), boost::lexical_cast<double, std::string>);
         m.push_back(std::move(row));
-       
     }
 
     return m;
 }
 
-std::pair<std::vector<int>, Matrix> readTestData(const std::filesystem::path& file, const std::string& delimiter){
-    std::ifstream stream { file };
-    std::string   line;
-    Matrix        m;
+std::pair<std::vector<int>, Matrix> readTestData(const std::filesystem::path& file, const std::string& delimiter)
+{
+    std::ifstream    stream { file };
+    std::string      line;
+    Matrix           m;
     std::vector<int> classes;
     std::size_t      counter = 0;
     while (std::getline(stream, line)) {
@@ -51,34 +51,36 @@ std::pair<std::vector<int>, Matrix> readTestData(const std::filesystem::path& fi
         ++counter;
     }
 
-    return {std::move(classes), std::move(m)};
+    return { std::move(classes), std::move(m) };
 }
 
-class Model{
+class Model
+{
 public:
-    Model(Matrix matrix) {
-        for (auto& row : matrix){
+    Model(Matrix matrix)
+    {
+        for (auto& row : matrix) {
             predictors_.push_back(std::make_unique<LinregPredictor>(std::move(row)));
         }
     }
 
-    int predictClass(const Row& row){
+    int predictClass(const Row& row)
+    {
         double probability    = std::numeric_limits<double>::lowest();
-        int predictedClass = -1;
-        for(int i = 0; i < predictors_.size(); ++i){
+        int    predictedClass = -1;
+        for (int i = 0; i < predictors_.size(); ++i) {
             auto result = predictors_[i]->predict(row);
-            if(result > probability){
-                probability = result;
+            if (result > probability) {
+                probability    = result;
                 predictedClass = i;
             }
-
         }
 
         return predictedClass;
     }
 
-        private:
-            std::vector<std::unique_ptr<LinregPredictor>> predictors_;
+private:
+    std::vector<std::unique_ptr<LinregPredictor>> predictors_;
 };
 
 int main(int argc, char* argv[])
@@ -92,29 +94,29 @@ int main(int argc, char* argv[])
     std::filesystem::path modelFile { argv[2] };
 
     std::cout << "Reading test data...\n";
-    auto [classes, data]  = readTestData(testFile, ",");
+    auto [classes, data] = readTestData(testFile, ",");
     std::cout << "Creating model...\n";
     Model model { readCoefficientsMatrix(modelFile, " ") };
 
     std::cout << "Inference...\n";
     std::vector<int> predictions;
-    std::size_t counter = 0;
-    for(const auto& row : data){
+    std::size_t      counter = 0;
+    for (const auto& row : data) {
         predictions.push_back(model.predictClass(row));
-        if(counter % 1000 == 0){
+        if (counter % 1000 == 0) {
             std::cout << "Testing case " << counter << '\n';
         }
         ++counter;
     }
 
     std::size_t errors = 0;
-    for(std::size_t i = 0; i < predictions.size(); ++i){
-        if(predictions[i] != classes[i]){
+    for (std::size_t i = 0; i < predictions.size(); ++i) {
+        if (predictions[i] != classes[i]) {
             ++errors;
         }
     }
 
-    double accuracy = errors == 0 ? 100. : (predictions.size() - errors)*100./predictions.size();
+    double accuracy = errors == 0 ? 100. : (predictions.size() - errors) * 100. / predictions.size();
     std::cout << "Accuracy: " << accuracy << '\n';
     return 0;
 }
